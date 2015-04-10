5741e5e7fbad336af14cba3ca59b73d475039314
Sebastian Roth <sebastian.roth@gmail.com>
2015-04-10 11:15:38 +0800

    Update WifiApManager.java
    Typo.

--- src/com/whitebyte/wifihotspotutils/WifiApManager.java
+++ src/com/whitebyte/wifihotspotutils/WifiApManager.java
@@ -128,7 +128,7 @@ public class WifiApManager {
 	 * @param onlyReachables {@code false} if the list should contain unreachable (probably disconnected) clients, {@code true} otherwise
 	 * @param finishListener, Interface called when the scan method finishes
 	 */
-	public void getClientList(boolean onlyReachables, FinishScanListener finishListner) {
+	public void getClientList(boolean onlyReachables, FinishScanListener finishListener) {
 		getClientList(onlyReachables, 300, finishListner );
 	}
 
